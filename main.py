5ee3dd709697bf3c7adf11e6e8be5a55b6e5b812
flaviamedici <flaviaomedici@gmail.com>
2026-07-15 09:29:12 -0700

    Adding doc string and Testing the module

---
+++ main.py
@@ -0,0 +1,6 @@
+import invoice
+
+invoice.generate("invoices", "output", "pythonhow.png",
+                 "product_id", "product_name", "amount_purchased",
+                 "price_per_unit", "total_price")
+
